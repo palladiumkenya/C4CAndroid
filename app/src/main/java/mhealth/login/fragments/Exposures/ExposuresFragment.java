4d30880e1050470f54f4a8852d62fa7d5bb8646b
Dennis Muoki <muoki@Denniss-MacBook-Air.local>
2020-03-03 11:01:22 +0300

    refactor fragments

--- app/src/main/java/mhealth/login/fragments/Exposures/ExposuresFragment.java
+++ app/src/main/java/mhealth/login/fragments/Exposures/ExposuresFragment.java
@@ -1,5 +1,6 @@
 package mhealth.login.fragments.Exposures;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,58 +9,54 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.fxn.stash.Stash;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 import mhealth.login.R;
+import mhealth.login.dependencies.Constants;
+import mhealth.login.models.User;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ExposuresFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class ExposuresFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private Unbinder unbinder;
+    private View root;
+    private Context context;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private User loggedInUser;
 
-    public ExposuresFragment() {
-        // Required empty public constructor
+    @Override
+    public void onAttach(Context ctx) {
+        super.onAttach(ctx);
+        this.context = ctx;
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ExposuresFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ExposuresFragment newInstance(String param1, String param2) {
-        ExposuresFragment fragment = new ExposuresFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_exposures, container, false);
+        root =  inflater.inflate(R.layout.fragment_exposures, container, false);
+        unbinder = ButterKnife.bind(this, root);
+
+        loggedInUser = (User) Stash.getObject(Constants.LOGGED_IN_USER, User.class);
+
+
+
+        return root;
     }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
 }
--- app/src/main/java/mhealth/login/fragments/Reasources/ProtocolsTabFragment.java
+++ app/src/main/java/mhealth/login/fragments/Reasources/ProtocolsTabFragment.java
@@ -1,5 +1,6 @@
 package mhealth.login.fragments.Reasources;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,58 +9,54 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.fxn.stash.Stash;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 import mhealth.login.R;
+import mhealth.login.dependencies.Constants;
+import mhealth.login.models.User;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProtocolsTabFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class ProtocolsTabFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private Unbinder unbinder;
+    private View root;
+    private Context context;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private User loggedInUser;
 
-    public ProtocolsTabFragment() {
-        // Required empty public constructor
+    @Override
+    public void onAttach(Context ctx) {
+        super.onAttach(ctx);
+        this.context = ctx;
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProtocolsTabFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProtocolsTabFragment newInstance(String param1, String param2) {
-        ProtocolsTabFragment fragment = new ProtocolsTabFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_protocols_tab, container, false);
+        root = inflater.inflate(R.layout.fragment_protocols_tab, container, false);
+        unbinder = ButterKnife.bind(this, root);
+
+        loggedInUser = (User) Stash.getObject(Constants.LOGGED_IN_USER, User.class);
+
+
+
+        return root;
     }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
 }
